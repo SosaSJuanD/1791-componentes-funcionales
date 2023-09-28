0e7ee70c9c1659a217cb89c7ae7ebb1d110bb3e6
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-28 16:40:51 -0500

    Primeros Componentes con Material UI

--- src/componentes/FormSingUp.jsx
+++ src/componentes/FormSingUp.jsx
@@ -1,20 +1,43 @@
 import Button from "@mui/material/Button"
-
-
+import TextField  from "@mui/material/TextField"
+import  Switch  from "@mui/material/Switch"
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 function FormSingUp(){
     return <form>
-        <label>Nombre</label>
-        <input type="text"></input>
-        <label>Apellido</label>
-        <input type="text"></input>
-        <label>Correo Electronico</label>
-        <input type="text"></input>
-        <label>Promociones</label>
-        <input type="checkbox"></input>
-        <label>Novedades</label>
-        <input type="checkbox"></input>
-
+        <TextField 
+            id="name" 
+            label="Nombre" 
+            variant="outlined"
+            fullWidth
+            margin="normal"
+        />
+        <TextField 
+            id="lastName" 
+            label="Apellido" 
+            variant="outlined"
+            fullWidth
+            margin="normal"
+        /> 
+        <TextField 
+            id="email" 
+            label="Correo electronico" 
+            variant="outlined"
+            fullWidth
+            margin="normal"
+        />  
+        <FormGroup>
+            <FormControlLabel
+                control= {<Switch defaultChecked/>}
+                label="Promociones"
+            />
+            <FormControlLabel
+                control= {<Switch defaultChecked/>}
+                label="Novedades"
+            />
+        </FormGroup>
+        
         <Button variant="contained">Registrarse</Button>
     </form>
 }
