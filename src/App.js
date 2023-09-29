fe53f939bb83cf8e9c3cfebc587dbf90ae133198
SosaSJuanD <sosasjuandavid@gmail.com>
2023-09-28 21:52:09 -0500

    manejo de informacion

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import FormSingUp from './componentes/FormSingUp';
 import Container  from '@mui/material/Container';
 import Typography  from '@mui/material/Typography';
 function App() {
+
+  const handleSubmit = (valores)=>{
+    console.log("APPJS: ", valores)
+  }
+
   return (
     <Container component="section" maxWidth="sm">
       <Typography variant='h3' align='center' component= "h1">Formulario Registro</Typography>
-      <FormSingUp/>
+      <FormSingUp handleSubmit={handleSubmit}/>
     </Container>
   );
 }
--- src/componentes/FormSingUp.jsx
+++ src/componentes/FormSingUp.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Button from "@mui/material/Button"
 import TextField  from "@mui/material/TextField"
 import  Switch  from "@mui/material/Switch"
@@ -6,33 +6,41 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
 
-function FormSingUp(){
+function FormSingUp({handleSubmit}){
     const[name, setName] = useState("")
-    useEffect(()=>{
-        console.log("name cambio", name)
-
-    },[name])
+    const[lastName, setLastName] = useState("")
+    const[email, setEmail] = useState("")
+    const[prom, setProm] = useState(true)
+    const[nov, setNov] = useState(false)
     
-    return <form>
+
+    return <form onSubmit={(e)=>{
+        e.preventDefault()
+        handleSubmit({
+            name, lastName, email, prom, nov
+        })
+    }}>
         <TextField 
             id="name" 
             label="Nombre" 
             variant="outlined"
             fullWidth
             margin="normal"
+            value={name}
             onChange={(e)=>{
                     setName(e.target.value)
-                    console.log(e.target.value)
                 }
             }
-        value={name}
         />
+
         <TextField 
             id="lastName" 
             label="Apellido" 
             variant="outlined"
             fullWidth
             margin="normal"
+            value={lastName}
+            onChange={(e)=> setLastName(e.target.value)}
         /> 
         <TextField 
             id="email" 
@@ -40,19 +48,27 @@ function FormSingUp(){
             variant="outlined"
             fullWidth
             margin="normal"
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
         />  
         <FormGroup>
             <FormControlLabel
-                control= {<Switch defaultChecked/>}
+                control= {<Switch
+                    checked={prom}
+                    onChange={(e)=> setProm(e.target.checked)}
+                />}
                 label="Promociones"
             />
             <FormControlLabel
-                control= {<Switch defaultChecked/>}
+                control= {<Switch
+                    checked={nov}
+                    onChange={(e)=> setNov(e.target.checked)}
+                />}
                 label="Novedades"
             />
         </FormGroup>
         
-        <Button variant="contained">Registrarse</Button>
+        <Button variant="contained" type="submit">Registrarse</Button>
     </form>
 }
 
